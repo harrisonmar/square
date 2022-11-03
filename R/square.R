ef4441a2362d18c5ff6d5dbab694204856c73dc2
Harrison Mar <harrisonmar95@gmail.com>
2022-11-03 09:51:41 -0700

    adding square() function w documentation

---
+++ R/square.R
@@ -0,0 +1,15 @@
+#' Squares a number or vector of numbers
+#'
+#' @param x the number or vector to be squared
+#'
+#' @return the square of x
+#' @export
+#'
+#' @examples
+#' square(10) # should return 100
+#' square(c(2,4)) # should return c(4,16)
+#'
+
+square <- function (x) {
+  x^2
+}
